20599cfe942dd6ca216082c215d34240d8a0ac13
hondapowerdd <william.meng@mail.utoronto.ca>
2025-04-01 04:39:03 -0400

    1

---
+++ personal_website/app/csc309-e11/page.tsx
@@ -0,0 +1,8 @@
+export default function TesterPage() {
+    return (
+        <div id="auto-tester">
+            Hi. I am mengzhi7, and I have learned how to develop a full-stack web app
+            and deploy it!
+        </div>
+    );
+}
